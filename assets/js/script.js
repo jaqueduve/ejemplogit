741c0c5f76291de69e996f5f97c8a98dde1f7e7c
Juan Jaque <ing.ambiental.jaque@gmail.com>
2022-04-27 19:52:33 -0400

    prueba_modulo_5

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    
+    $("a").click(function () {
+        var gato = this.hash
+        
+        $("html, body").animate(
+            {
+                scrollTop: $(gato).offset().top
+            },
+            1200
+
+
+        )
+
+    })
+
+
+})
